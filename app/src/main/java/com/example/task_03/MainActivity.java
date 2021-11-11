c427f08dbc6de5222c02ac51c7721d834fbbcc58
Usman <bitf18a536@pucit.edu.pk>
2021-11-11 22:48:04 +0500

    Repostry Link Added

--- app/src/main/java/com/example/task_03/MainActivity.java
+++ app/src/main/java/com/example/task_03/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.task_03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        repo=findViewById(R.id.button);
+        repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Uri uri = Uri.parse("https://github.com/UsmanKhalid95/Task_03.git");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
