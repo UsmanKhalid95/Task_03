da385ec69c61efe3b6473beb404a0f34429efcba
Usman <bitf18a536@pucit.edu.pk>
2021-11-15 21:54:27 +0500

    Activity 2 wrok

--- app/src/main/java/com/example/task_03/MainActivity2.java
+++ app/src/main/java/com/example/task_03/MainActivity2.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    String [] ArabicLetters={"ا","ب","ت","ث","ج","ح","خ","د","ذ","ر","ز","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ك","ل","م","ن","ه","و","ي"};
+    Random rNum=new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        int number= rNum.nextInt(28);
 
 
 
